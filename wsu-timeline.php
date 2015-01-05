938d2e974353a799295a768c358b3efb1a30a779
jeremyfelt <jeremy.felt@gmail.com>
2015-01-05 14:58:34 -0800

    Add initial plugin files

---
+++ wsu-timeline.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: WSU Timeline (125th)
+Version: 0.0.1
+Plugin URI: http://web.wsu.edu
+Description: Provides the content requirements for the display of a timeline.
+Author: washingtonstateuniversity, jeremyfelt
+Author URI: http://web.wsu.edu
+*/
+
+class WSU_Timeline {
+	/**
+	 * @var string Current plugin version for cache breaking
+	 */
+	var $version = '0.0.1';
+	
+	/**
+	 * Setup plugin.
+	 */
+	public function __construct() {
+
+	}
+}
+new WSU_Timeline();
